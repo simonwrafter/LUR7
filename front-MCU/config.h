463030f24c152c6d1dc6cabbd9f5cd2e2a84f71b
Simon Wrafter <simon.wrafter@gmail.com>
2015-03-08 18:25:46 +0100

    documenting front

--- front-MCU/config.h
+++ front-MCU/config.h
@@ -16,21 +16,51 @@
  * / along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/*! \file front-MCU/config.h
+ * The \ref front-config defines inputs and constants used on the front MCU.
+ *
+ * All code is released under the GPLv3 license.
+ *
+ * \see \ref front-MCU/main.c
+ * \see <http://www.gnu.org/copyleft/gpl.html>
+ * \author Simon Wrafter
+ * \copyright GNU Public License v3.0
+ *
+ * \defgroup front-config Front MCU - Configuration File
+ * The front MCU has several inputs for measuring the behaviour of the LUR7. In
+ * \ref front-MCU/config.h the setup of the MCU is defined. 
+ *
+ * \see \ref front-main
+ * \see <http://www.gnu.org/copyleft/gpl.html>
+ * \author Simon Wrafter
+ * \copyright GNU Public License v3.0
+ */
+
 #ifndef _CONFIG_H_
 #define _CONFIG_H_
 
 //Constants
+//! The value above which the brakes are considered to have been applied.
 #define BRAKES_ON			100
 
 //Inputs
+//! Input for speed measurment of front right wheel.
 #define WHEEL_R				IN1
+//! Input for speed measurment of front left wheel.
 #define WHEEL_L				IN2
+// unused
 //#define					IN3
+//! Input (analog) for measurment of brake pressure.
 #define BRAKE_PRESSURE	ADC_IN4
+// unused
 //#define					IN5
+//! Input (analog) for measurment of steering wheel angle.
 #define STEERING_WHEEL	ADC_IN6
+// unused
 //#define					IN7
+//! Input (analog) for measurment of front right suspension.
 #define SUSPENSION_R	ADC_IN8
+//! Input (analog) for measurment of front left suspension.
 #define SUSPENSION_L	ADC_IN9
 
 //Outputs
