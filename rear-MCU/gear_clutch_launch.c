03e75f7182a45658d095edd356a8128a2fccc2a8
Simon Wrafter <simon.wrafter@gmail.com>
2015-04-04 02:01:13 +0200

    todo: log timing info

--- rear-MCU/gear_clutch_launch.c
+++ rear-MCU/gear_clutch_launch.c
@@ -1,7 +1,7 @@
 /*
  * gear_clutch_launch.c - A collection of functions to setup and ease the use of the LUR7 PCB
  * Copyright (C) 2015  Simon Wrafter <simon.wrafter@gmail.com>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -17,7 +17,7 @@
  */
 
 /*! \file gear_clutch_launch.c
- * \ref gear_clutch_launch contains functions for gear changing, clutch and 
+ * \ref gear_clutch_launch contains functions for gear changing, clutch and
  * launch control on the rear MCU used in the LUR7.
  *
  * All code is released under the GPLv3 license.
@@ -31,7 +31,7 @@
  * \copyright GNU Public License v3.0
  *
  * \defgroup gear_clutch_launch Rear MCU - Gear, Clutch and Launch Control
- * \ref gear_clutch_launch.c controls gear changing procedures, clutch 
+ * \ref gear_clutch_launch.c controls gear changing procedures, clutch
  * positioning and launch control.
  *
  * \see \ref gear_clutch_launch.c
@@ -147,11 +147,11 @@ void set_current_gear(uint8_t gear) {
 
 //! Change gear up, part 1
 /*!
- * Changing up a gear is the most intricate of the gear changing routines. Shift 
- * cut needs to be applied and allowed to take effect before the solenoid is 
- * engaged. There being two long delays to consider in this routine means 
+ * Changing up a gear is the most intricate of the gear changing routines. Shift
+ * cut needs to be applied and allowed to take effect before the solenoid is
+ * engaged. There being two long delays to consider in this routine means
  * the more straight forward method used in \ref gear_down is not applicable.
- * 
+ *
  * The steps to change gear are thus:
  * <ol>
  * <li> In \ref gear_up function.
@@ -174,16 +174,16 @@ void set_current_gear(uint8_t gear) {
  *   <li> clear busy flag.
  *   </ol>
  * </ol>
- * 
- * The flag \ref busy is set at the start of each operation and cleared once 
+ *
+ * The flag \ref busy is set at the start of each operation and cleared once
  * timer0 finishes, hindering more than one action at a time. If \ref busy is set
  * when the function is triggered no gear change will happen.
- * 
+ *
  * If the car is in fifth gear, trying to shift up will have no effect. However,
  * this is only a feature as long as the DTA is not in fail mode.
- * 
+ *
  * \todo special case for neutral -> 1st.
- * 
+ *
  * \param current_gear allows the controler to behave in the most appropriate manner
  */
 void gear_up() {
@@ -197,19 +197,19 @@ void gear_up() {
 
 //! Change gear down
 /*!
- * This function assumes that the clutch is engaged when triggered. The flag 
- * \ref busy is set at the start of each operation and cleared once timer0 
+ * This function assumes that the clutch is engaged when triggered. The flag
+ * \ref busy is set at the start of each operation and cleared once timer0
  * finishes, hindering more than one action at a time. If \ref busy is set when
- * the function is triggered no gear change will happen. 
- * 
- * The procedure is straight forward, \ref gear_down_flag is set and the 
+ * the function is triggered no gear change will happen.
+ *
+ * The procedure is straight forward, \ref gear_down_flag is set and the
  * \ref GEAR_DOWN output set to activate the solenoid for the time defined in
- * \ref GEAR_DOWN_DELAY. Once \ref timer0_isr_stop is triggered, the output is 
+ * \ref GEAR_DOWN_DELAY. Once \ref timer0_isr_stop is triggered, the output is
  * reset and the flags cleared.
- * 
+ *
  * If the car is in first gear, trying to shift down will have no effect. However,
  * this is only a feature as long as the DTA is not in fail mode.
- * 
+ *
  * \param current_gear allows the controler to behave in the most appropriate manner
  */
 void gear_down() {
@@ -249,25 +249,25 @@ static void end_gear_change(void) {
 
 //! Find neutral gear
 /*!
- * This function assumes that the clutch is engaged when triggered. The flag 
- * \ref busy is set at the start of each operation and cleared once timer0 
+ * This function assumes that the clutch is engaged when triggered. The flag
+ * \ref busy is set at the start of each operation and cleared once timer0
  * finishes, hindering more than one action at a time. If \ref busy is set when
  * the function is triggered no gear change will happen.
- * 
- * The procedure is fairly straight forward, depending on the currently selected 
+ *
+ * The procedure is fairly straight forward, depending on the currently selected
  * gear (1 or 2, others disregarded) the solenoid is set to give a pulse in the
- * appropriate direction. \ref neutral_up_flag (from gear 1) or 
- * \ref neutral_down_flag (from gear 2) is set and the \ref GEAR_UP (1) or 
+ * appropriate direction. \ref neutral_up_flag (from gear 1) or
+ * \ref neutral_down_flag (from gear 2) is set and the \ref GEAR_UP (1) or
  * \ref GEAR_DOWN (2) output set to activate the solenoid for the time defined in
- * either \ref NEUTRAL_UP_DELAY (1) or \ref NEUTRAL_DOWN_DELAY (2). Once 
+ * either \ref NEUTRAL_UP_DELAY (1) or \ref NEUTRAL_DOWN_DELAY (2). Once
  * \ref timer0_isr_stop is triggered, the output is reset and the flags cleared.
- * 
+ *
  * Should the DTA be in fail mode the neutral finder will be active in all gears,
  * it will assume being in first gear and try to find neutral above the current gear.
- * 
+ *
  * All delay times are static and no adaptive scaling of the force to find the
  * neutral is implemented.
- * 
+ *
  * \param current_gear allows the controler to behave in the most appropriate manner
  */
 void gear_neutral_single() {
@@ -364,6 +364,7 @@ static void neutral_repeat_worker_linear(void) {
 		timer0_start(neutral_down_try_time += NEUTRAL_DELAY_ADJUST);
 	} else {
 		busy = FALSE;
+		//tx_can_setup send time information of sucessfull neutral search
 		return;
 	}
 	last_gear = current_gear;
@@ -431,6 +432,7 @@ static void neutral_repeat_worker_binary(void) {
 		timer0_start(neutral_down_try_time);
 	} else {
 		busy = FALSE; //assume in neutral. (or in third, but that really shouldn't happen.)
+		//tx_can_setup send time information of sucessfull neutral search
 		return;
 	}
 
@@ -453,17 +455,17 @@ static void neutral_repeat_stabiliser_binary(void) {
 
 //! Position the clutch servo.
 /*!
- * The sensor positioned in the steering wheel givves an analog signal which is 
- * converted to a 10 bit digital number (0 - 1023). As the clutch paddle moves 
- * in both directions the position of the clutch is calculated as the displacement 
- * from the mid-point (512). To avoid having the servo move due to any vibrations 
- * that find their way in to the steering wheel the paddle must be moved by a 
- * minimum threshold angle. A maximum value also exists to prevent over extension 
- * of the clutch. Between the lower threshold and maximum values a linear mapping 
+ * The sensor positioned in the steering wheel givves an analog signal which is
+ * converted to a 10 bit digital number (0 - 1023). As the clutch paddle moves
+ * in both directions the position of the clutch is calculated as the displacement
+ * from the mid-point (512). To avoid having the servo move due to any vibrations
+ * that find their way in to the steering wheel the paddle must be moved by a
+ * minimum threshold angle. A maximum value also exists to prevent over extension
+ * of the clutch. Between the lower threshold and maximum values a linear mapping
  * function transforms the ADC value to a useable PWM dutycycle for the servo.
- * 
+ *
  * \todo possibly divide the range into three zones.
- * 
+ *
  * \param pos the angle of the clutch paddle.
  */
 void clutch_set(uint16_t pos) {
